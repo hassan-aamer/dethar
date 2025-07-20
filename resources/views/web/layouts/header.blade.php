be40d4076577ce72136275ad12f2e11f32ded5d8
hassan-aamer <hassanaamer048@gmail.com>
2025-07-20 15:35:29 +0300

    Enhance header navigation with dropdown menus and update button text for clarity

--- resources/views/web/layouts/header.blade.php
+++ resources/views/web/layouts/header.blade.php
@@ -11,14 +11,38 @@
               <ul>
                   <li><a href="index.html" class="active">Home<br></a></li>
                   <li><a href="about.html">About</a></li>
-                  <li><a href="services.html">Services</a></li>
+                  <li class="dropdown"><a href="#"><span>What We Do</span> <i
+                              class="bi bi-chevron-down toggle-dropdown"></i></a>
+                      <ul>
+                          <li><a href="#">Dropdown 1</a></li>
+                      </ul>
+                  </li>
+                  <li class="dropdown"><a href="#"><span>Value add</span> <i
+                              class="bi bi-chevron-down toggle-dropdown"></i></a>
+                      <ul>
+                          <li><a href="#">Dropdown 1</a></li>
+                      </ul>
+                  </li>
+                  <li class="dropdown"><a href="#"><span>Our products</span> <i
+                              class="bi bi-chevron-down toggle-dropdown"></i></a>
+                      <ul>
+                          <li><a href="#">Dropdown 1</a></li>
+                      </ul>
+                  </li>
+                  <li class="dropdown"><a href="#"><span>Blogs</span> <i
+                              class="bi bi-chevron-down toggle-dropdown"></i></a>
+                      <ul>
+                          <li><a href="#">Dropdown 1</a></li>
+                      </ul>
+                  </li>
+                  {{-- <li><a href="services.html">Services</a></li>
                   <li><a href="pricing.html">Pricing</a></li>
-                  <li><a href="contact.html">Contact</a></li>
+                  <li><a href="contact.html">Contact</a></li> --}}
               </ul>
               <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
           </nav>
 
-          <a class="btn-getstarted" href="get-a-quote.html">Get a Quote</a>
+          <a class="btn-getstarted" href="get-a-quote.html">Get in touch</a>
 
       </div>
   </header>
--- resources/views/web/pages/home.blade.php
+++ resources/views/web/pages/home.blade.php
@@ -4,69 +4,22 @@
     <main class="main">
 
         <!-- Hero Section -->
-        <section id="hero" class="hero section dark-background">
+        <section style="height: 100vh;" id="hero" class="hero section dark-background">
 
             <img src="{{ App\Helpers\Image::getMediaUrl(App\Models\Setting::first(), 'baners') }}" alt=""
                 class="hero-bg" data-aos="fade-in">
 
             <div class="container">
                 <div class="row gy-4 d-flex justify-content-between">
-                    <div class="col-lg-6 order-2 order-lg-1 d-flex flex-column justify-content-center">
-                        <h2 data-aos="fade-up">{{ setting('title') ?? '' }}</h2>
-                        <p data-aos="fade-up" data-aos-delay="100">{{ setting('description') ?? '' }}</p>
-
-                        {{-- <form action="#" class="form-search d-flex align-items-stretch mb-3" data-aos="fade-up"
-                            data-aos-delay="200">
-                            <input type="text" class="form-control" placeholder="Your ZIP code or City. e.g. New York">
-                            <button type="submit" class="btn btn-primary">Search</button>
-                        </form> --}}
-
-                        {{-- <div class="row gy-4" data-aos="fade-up" data-aos-delay="300">
-
-                            <div class="col-lg-3 col-6">
-                                <div class="stats-item text-center w-100 h-100">
-                                    <span data-purecounter-start="0" data-purecounter-end="232"
-                                        data-purecounter-duration="0" class="purecounter">232</span>
-                                    <p>Clients</p>
-                                </div>
-                            </div><!-- End Stats Item -->
-
-                            <div class="col-lg-3 col-6">
-                                <div class="stats-item text-center w-100 h-100">
-                                    <span data-purecounter-start="0" data-purecounter-end="521"
-                                        data-purecounter-duration="0" class="purecounter">521</span>
-                                    <p>Projects</p>
-                                </div>
-                            </div><!-- End Stats Item -->
-
-                            <div class="col-lg-3 col-6">
-                                <div class="stats-item text-center w-100 h-100">
-                                    <span data-purecounter-start="0" data-purecounter-end="1453"
-                                        data-purecounter-duration="0" class="purecounter">1453</span>
-                                    <p>Support</p>
-                                </div>
-                            </div><!-- End Stats Item -->
-
-                            <div class="col-lg-3 col-6">
-                                <div class="stats-item text-center w-100 h-100">
-                                    <span data-purecounter-start="0" data-purecounter-end="32" data-purecounter-duration="0"
-                                        class="purecounter">32</span>
-                                    <p>Workers</p>
-                                </div>
-                            </div><!-- End Stats Item -->
-
-                        </div> --}}
-
+                    <div class="col-lg-9 order-2 order-lg-1 d-flex flex-column justify-content-center">
+                        <h2 style="margin-top: 60px;" data-aos="fade-up">{{ setting('title') ?? '' }}</h2>
+                        <p style="margin-top: 20px;" data-aos="fade-up" data-aos-delay="100">{{ setting('description') ?? '' }}</p>
                     </div>
-
-                    {{-- <div class="col-lg-5 order-1 order-lg-2 hero-img" data-aos="zoom-out">
-                        <img src="{{ asset('web/img/hero-img.svg') }}" class="img-fluid mb-3 mb-lg-0" alt="">
-                    </div> --}}
-
                 </div>
             </div>
 
-        </section><!-- /Hero Section -->
+        </section>
+        <!-- /Hero Section -->
 
         <!-- Featured Services Section -->
         {{-- <section id="featured-services" class="featured-services section">
@@ -200,7 +153,7 @@
         @endif
 
         <!-- Call To Action Section -->
-        <section id="call-to-action" class="call-to-action section dark-background">
+        {{-- <section id="call-to-action" class="call-to-action section dark-background">
 
             <img src="{{ App\Helpers\Image::getMediaUrl(App\Models\Setting::first(), 'callToActions') }}" alt="">
 
@@ -218,7 +171,8 @@
                 </div>
             </div>
 
-        </section><!-- /Call To Action Section -->
+        </section> --}}
+        <!-- /Call To Action Section -->
 
 @if ($result['features']->count())
 <!-- Features Section -->
