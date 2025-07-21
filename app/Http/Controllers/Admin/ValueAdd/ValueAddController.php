2df0eb9a421cc06525ef6090c1abc56d4a634cdf
hassan-aamer <hassanaamer048@gmail.com>
2025-07-21 23:05:40 +0300

    Add controllers for Blog, Page, ValueAdd, and WhatWeDo with initial structure

---
+++ app/Http/Controllers/Admin/ValueAdd/ValueAddController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Admin\ValueAdd;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ValueAddController extends Controller
+{
+    //
+}
