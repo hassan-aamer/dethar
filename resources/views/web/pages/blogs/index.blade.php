bf67f185cfbba5aea0a4edd1f6c1965a02083bfc
hassan-aamer <hassanaamer048@gmail.com>
2025-07-25 18:02:38 +0300

    update

--- resources/views/web/pages/blogs/index.blade.php
+++ resources/views/web/pages/blogs/index.blade.php
@@ -35,18 +35,26 @@
             <div class="container">
                 <div class="row gy-4">
 
-                    @foreach ($result as $item)
-                        <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="200">
-                            <div class="card">
-                                <div class="card-img">
-                                    <img src="{{ App\Helpers\Image::getMediaUrl($item, 'blogs') }}" alt=""
-                                        class="img-fluid">
-                                </div>
-                                <h3><a href="#" class="stretched-link">{{ $item->title ?? '' }}</a></h3>
-                                <p>{{ $item->description ?? '' }}</p>
+                    @if ($result->isEmpty())
+                        <div class="d-flex justify-content-center align-items-center">
+                            <div class="col-12 text-center">
+                                <h1 style="color: #EA9323;">No blog posts found.</h1>
                             </div>
                         </div>
-                    @endforeach
+                    @else
+                        @foreach ($result as $item)
+                            <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="200">
+                                <div class="card">
+                                    <div class="card-img">
+                                        <img src="{{ App\Helpers\Image::getMediaUrl($item, 'blogs') }}" alt=""
+                                            class="img-fluid">
+                                    </div>
+                                    <h3><a href="#" class="stretched-link">{{ $item->title ?? '' }}</a></h3>
+                                    <p>{{ $item->description ?? '' }}</p>
+                                </div>
+                            </div>
+                        @endforeach
+                    @endif
 
                 </div>
             </div>
