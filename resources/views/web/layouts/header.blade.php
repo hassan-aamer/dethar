e8ca579e1edc2beae89ebe036c66e700c3926d81
hassan-aamer <hassanaamer048@gmail.com>
2025-07-30 15:56:36 +0300

    `Removed 4 image files and updated header.blade.php layout with new styles and logo`

--- resources/views/web/layouts/header.blade.php
+++ resources/views/web/layouts/header.blade.php
@@ -1,44 +1,46 @@
-  <header id="header" style="background-color: #2E6344;" class="header d-flex align-items-center fixed-top">
+  <header id="header" style="background-color: #ede4d9;font-weight: bold;"
+      class="header d-flex align-items-center fixed-top">
       <div class="container-fluid container-xl position-relative d-flex align-items-center">
 
           <a href="{{ route('home') }}" class="logo d-flex align-items-center me-auto">
               <!-- Uncomment the line below if you also wish to use an image logo -->
-              {{-- <img src="{{ App\Helpers\Image::getMediaUrl(App\Models\Setting::first(), 'logo') }}" alt=""> --}}
-              <h1 class="sitename" style="color: #EA9323;">{{ setting('name') ?? '' }}</h1>
+              <img style="width: 100px;" src="{{ asset('web/img/l5.png') }}" alt="logo">
+              {{-- <h1 class="sitename" style="color: #EA9323;">{{ setting('name') ?? '' }}</h1> --}}
           </a>
 
           <nav id="navmenu" class="navmenu">
               <ul>
-                  <li><a href="{{ route('home') }}"
-                          class=" {{ request()->routeIs('home') ? 'active' : '' }}">Home<br></a></li>
-                  <li><a href="{{ route('about') }}"
-                          class=" {{ request()->routeIs('about') ? 'active' : '' }}">About</a></li>
-                  <li class="dropdown"><a href="#"><span>What We Do</span> <i
-                              class="bi bi-chevron-down toggle-dropdown"></i></a>
+                  <li><a href="{{ route('home') }}" class=" {{ request()->routeIs('home') ? 'active' : '' }}"
+                          style="color: #EA9323;">Home<br></a></li>
+                  <li><a href="{{ route('about') }}" class=" {{ request()->routeIs('about') ? 'active' : '' }}"
+                          style="color: #EA9323;">About</a></li>
+                  <li class="dropdown"><a href="#"><span style="color: #EA9323;">What We Do</span> <i
+                              class="bi bi-chevron-down toggle-dropdown" style="color: #EA9323;"></i></a>
                       <ul>
                           @foreach (WhatWeDoCategories() as $category)
                               <li><a href="{{ route('what', $category->id) }}">{{ $category->title ?? '' }}</a></li>
                           @endforeach
                       </ul>
                   </li>
-                  <li class="dropdown"><a href="#"><span>Added value</span> <i
-                              class="bi bi-chevron-down toggle-dropdown"></i></a>
+                  <li class="dropdown"><a href="#"><span style="color: #EA9323;">Added value</span> <i
+                              class="bi bi-chevron-down toggle-dropdown" style="color: #EA9323;"></i></a>
                       <ul>
                           @foreach (ValueAddCategories() as $category)
                               <li><a href="{{ route('value', $category->id) }}">{{ $category->title ?? '' }}</a></li>
                           @endforeach
                       </ul>
                   </li>
-                  <li class="dropdown"><a href="#"><span>Our products</span> <i
-                              class="bi bi-chevron-down toggle-dropdown"></i></a>
+                  <li class="dropdown"><a href="#"><span style="color: #EA9323;">Our products</span> <i
+                              class="bi bi-chevron-down toggle-dropdown" style="color: #EA9323;"></i></a>
                       <ul>
                           @foreach (ProductCategories() as $category)
-                              <li><a href="{{ route('products', $category->id) }}">{{ $category->title ?? '' }}</a></li>
+                              <li><a href="{{ route('products', $category->id) }}">{{ $category->title ?? '' }}</a>
+                              </li>
                           @endforeach
                       </ul>
                   </li>
-                  <li class="dropdown"><a href="#"><span>Blogs</span> <i
-                              class="bi bi-chevron-down toggle-dropdown"></i></a>
+                  <li class="dropdown"><a href="#"><span style="color: #EA9323;">Blogs</span> <i
+                              class="bi bi-chevron-down toggle-dropdown" style="color: #EA9323;"></i></a>
                       <ul>
                           @foreach (BlogCategories() as $category)
                               <li><a href="{{ route('blog', $category->id) }}">{{ $category->title ?? '' }}</a></li>
@@ -49,7 +51,7 @@
                   <li><a href="pricing.html">Pricing</a></li>
                   <li><a href="contact.html">Contact</a></li> --}}
               </ul>
-              <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
+              <i style="color: #EA9323;" class="mobile-nav-toggle d-xl-none bi bi-list"></i>
           </nav>
 
           <a class="btn-getstarted" href="{{ route('contact') }}">Get in touch</a>
